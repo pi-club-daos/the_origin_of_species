0c4a067bd6f6824be859ebb4a250e37a672be2c7
thomas <thomas.chernaik@gmail.com>
2020-08-24 15:08:52 +0100

    added characteristics

--- game.py
+++ game.py
@@ -111,12 +111,10 @@ class Item:
 
 
 class Creature(Item):
-    def __init__(self, game, location, size, name, species, characteristics, generation, minPointsEnergy, maxEnergy, energyPerUnitSize, energyPerDistanceMoved, ID):
+    def __init__(self, game, location, size, name, species, characteristics, generation, minPointsEnergy, maxEnergy, ID):
         self.gender = random.randint(0,1)# male = 0 female = 1 only females can have offspring and require a male to do this
         self.ID = (ID, name)#this is used for the family tree and is seperate to the species ID which is the player name. This includes the creature name to make the family tree more readable
-        self.energyPerDistanceMoved = energyPerDistanceMoved
         Item.__init__(self, game, location, size)
-        self.energyPerUnitSize = energyPerUnitSize
         self.name = name#a randomly generated name for the creature
         self.age = 0#the age of the item. This doesn't matter for plants. Age is the number of ticks since the item was created.
         self.species = species#the player who owns this creature
@@ -128,40 +126,44 @@ class Creature(Item):
         self.maxEnergy = maxEnergy#the the maximum energy the creature can have
         self.isBaby = True#the creature is immobile when it is a baby
         self.nextLoc = ()#the location that the creature will move to when move is called
+        self.dist = 0
 
     def calculateMove(self, destination):
         #this function calculates the move for the creature. It will use pathfinding, and return the space that the player should move to based on the speed of the creature
         path = self.game.pathFind(self.location, destination)
-        self.nextLoc = path[self.characteristics["speed"]]#this will get the speed from the characteristics and move to that space in the path. This means speed is equal to the number of steps moved in one tick
+        self.dist = min(len(path), self.characteristics["speed"])
+        self.nextLoc = path[self.dist]#this will get the speed from the characteristics and move to that space in the path. This means speed is equal to the number of steps moved in one tick
 
     def move(self):
         #this function moves the creature based on the move from calculateMove. all creatures should be moved at the same time after decisions have all been made. The creatures energy should decrease more if it moves but should always be decreased here. The species points should increase if it has energy over minpointsenergy
-        self.energy -= self.location.dist(self.nextLoc) * self.energyPerDistanceMoved
+        self.energy -= self.dist * self.characteristics["energy per distance moved"] * self.size
         self.location = self.nextLoc
 
     def haveOffspring(self):
         #this function does the neccessary processes for if the creature wants to have offspring, including prompting the player to make decisions if it is the first offspring of its generation
-        offspring.append(self.species.addOffspring())
+        self.offspring.append(self.species.addOffspring(self.generation+1))
 
 
     def eat(self, item):
+        # this function does the necessary processes for if the creature eats something. the amount of energy added should be decided by the size of the item
         if(not self.characteristics["can eat poisonous plants"] and item.__name__ == "Plant"):#sees if it is eating a plant and the creature can be killed by poisonous plants
             if item.poisonous:
                 self.species.chat("system: %s ate a poisonous plant and died" % (self.name))
+                item.eaten()
                 self.dead()
 
-        self.species.updateChat("system: %s ate %s of species %s" % (self.name, other.name, other.species.ID))
-        self.energy += self.energyPerUnitSize * item.size
+        self.species.updateChat("system: %s ate %s of species %s" % (self.name, item.name, item.species.ID))
+        self.energy += self.characteristics["energy per unit size"] * item.size
         item.eaten()
         if self.energy > self.maxEnergy:
             self.energy == self.maxEnergy
-        #this function does the necessary processes for if the creature eats something. the amount of energy added should be decided by the size of the item
+
 
 
     def getInformation(self):
         #this function queries the isInSight of every item and returns a list of all that return true, along with the distance
         itemsInSight = []
-        distSquared = self.characteristics["view radius squared"]
+        distSquared = self.characteristics["maximum view dist squared"]
         for item in self.game.items:
             itemDist = item.isInSight(self.location)
             if itemDist > distSquared:
@@ -194,26 +196,53 @@ class Plant(Item):
     def eaten(self):
         # this function does the neccessary processes for if the creature is eaten. It will only be called by the creature that ate it
         self.game.plantIsEaten(self)
-        pass
 
 
 class Species:
 
     def __init__(self, ID, initialSize, game):
         self.game = game
+        self.creatureID = 0
         self.creatures = []#a list of all the living creatures in the species
         self.familytree = {}#a graph of all the dead creatures' IDs and
         self.chat = []#a chat containing all of the events specific to this species
         self.ID = ID#the ID of this species that the player who is controlling this species needs to make actions
-        self.characteristics = {"""some stuff needs to be in here"""}#a list of the characteristics of each generation
+        self.characteristics = [{"speed" : 0,#the distance that the creature can move per step i time
+                                "maximum view dist squared" : 0,#the maximum distance that the creature can see, squared
+                                "size" : 0,#the size of the creature
+                                 "size can eat" : 0,#the biggest size of creature that this creature can eat, as a ratio
+                                 "carnivorous":False,#if the creature can eat other creatures
+                                 "number of offspring" : 0,#the number of offspring that will be had by the creature when it has offspring
+                                 "time to grow up" : 100,#this is a magic number. also the number of steps in time it will take for the creature to be able to move
+                                 "energy per unit size" : 100,#this is a magic number. also the amount of energy that a different creature will gain when eating this creature. this should probably be fixed and not here.
+                                 "energy per distance moved" : 100,#this is a magic number. also the amount of energy that will be used by the creature when it is moving
+                                 "lifespan" : 0,#the number of steps in time after which the creature will die of old age
+                                 "camouflage" : 0,#the chance that another creature won't see this animal. this includes mates
+                                 "can recognise predators": False,#if the creature can see predators, and therefore run away
+                                 "can eat poison" : False,#if the creature can eat poisonous plants
+                                 "can see poison" : False#if the creature can see poisonous plants so it knows not to eat them
+                                }]#a list of the characteristics of each generation
         self.points = 0#the points of the species, used to purchase new characteristics. Characteristics cost more the more creatures there are
         self.size = [initialSize]#the size of members of the species as a list split by generations
         self.chatLastRead = time.time()
 
-    def addOffspring(self):
-        #adds a new offspring and tells the player they need to specify the characteristics of the next generation if necessary
-        pass
+    def addOffspring(self, generation, parent):
+        #adds a new offspring
+        if(len(self.characteristics) < generation):
+            #tell the server to ask the player to specify the characteristics for the next genreation. it will ignore this if it has already asked
+            return#this means that the player hasn't specified the characteristics for this generation yet so this child can't be had.
+        if(len(self.characteristics) == generation):#this means that this creature will belong to the youngest generation
+            #tell the server to ask the player to specify the characteristics for the next generation
+            pass
+        newCreature = Creature(self.game, parent.location, self.size[generation-1], self.generateRandomName(), self, self.characteristics[generation-1], generation, 50, 100, 0.1, 0.1, self.creatureID)#these are magic numbers and should be replaced
+        self.creatures.append(newCreature)
+        self.game.items.append(newCreature)
+        self.creatureID += 1
 
+
+    def generateRandomName(self):
+        #choose a random name
+        return
     def firstGeneration(self):
         #a function that creates the first genereation of creatures and returns a list of them. The location must be a space on the map with a value of zero
         pass
@@ -226,7 +255,7 @@ class Species:
         #sends a message to the global game chat, and therefore all players
         message = self.ID + ": " + message
         self.game.updateChat(message)
-        pass
+    
 
     def creatureIsKilled(self, creature):
         #removes the creature from creatures and adds it to the family tree
